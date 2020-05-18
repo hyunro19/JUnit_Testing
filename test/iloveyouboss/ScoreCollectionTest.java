9e19e46574b6cb00267b3735b0b07bda13b5a8e9
hyunro19 <hyunro19@gmail.com>
2020-05-18 15:12:10 +0900

    200518 | initial commit

---
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,26 @@
+package iloveyouboss;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.*;
+
+import org.junit.Test;
+
+public class ScoreCollectionTest {
+
+	@Test
+	public void answersArithmeticMeanOfTwoNumbers() {
+		// 준비
+		ScoreCollection collection = new ScoreCollection();
+		collection.add(()->5);
+		collection.add(()->7);
+		
+		// 실행
+		int actualResult = collection.arithmeticMean();
+		
+		// 단언
+		assertThat(actualResult, equalTo(6));		
+	}
+
+}
+
